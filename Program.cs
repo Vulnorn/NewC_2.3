6bb60852a72fd451467da9b2d23edece73cd6068
Vulnorn <mixadrbk31@gmail.com>
2024-10-22 14:28:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NewC_2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 5;
+            int maxNumber = 103;
+            int stepSequence = 7;
+            int number = minNumber;
+
+            while (number <= maxNumber)
+            {
+                Console.Write($"{number} ");
+                number = number + stepSequence;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
